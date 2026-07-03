d17163031d15fc48d91bcabceef91586f5222bc4
Raphael Ebalida <ebalidaraphael@gmail.com>
2026-07-03 15:48:59 +0800

    sample 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -189,7 +189,7 @@
                     @auth
                         <a href="{{ url('/dashboard') }}" class="px-5 py-2 text-gray-700 hover:text-indigo-600 font-medium transition">Dashboard - Anotehr test</a>
                     @else
-                        <a href="{{ route('login') }}" class="px-5 py-2 text-gray-700 hover:text-indigo-600 font-medium transition">Login</a>
+                        <a href="{{ route('login') }}" class="px-5 py-2 text-gray-700 hover:text-indigo-600 font-medium transition">Login - Test</a>
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="px-5 py-2 bg-gradient-to-r from-indigo-600 to-purple-600 text-white rounded-full font-medium hover:shadow-lg transition">
                                 Apply Now
